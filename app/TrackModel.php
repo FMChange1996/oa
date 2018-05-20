9d7229bec009b1eb87f2cae7e638eef08e5166fb
SAM <2772002035@qq.com>
2018-05-20 14:39:17 +0800

    增加监听提交功能

--- app/TrackModel.php
+++ app/TrackModel.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TrackModel extends Model
 {
+    use SoftDeletes;
     //绑定表
     protected $table = 'track';
 
